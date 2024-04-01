070264c52a23b1649b0a6f69412ed6278ee43f13
mononoken <ken.maeshima@pm.me>
2024-04-01 14:51:01 -0700

    Add relation_scope test for AdoptablePetPolicyTest

--- test/policies/adoptable_pet_policy_test.rb
+++ test/policies/adoptable_pet_policy_test.rb
@@ -5,8 +5,26 @@ class AdoptablePetPolicyTest < ActiveSupport::TestCase
   include PetRescue::PolicyAssertions
 
   def setup
-    @policy = -> { AdoptablePetPolicy.new(@pet, user: @user) }
     @user = build_stubbed(:user)
+    @policy = -> { AdoptablePetPolicy.new(@pet, user: @user) }
+  end
+
+  context "relation_scope" do
+    setup do
+      @policy = AdoptablePetPolicy.new(Pet, user: @user)
+      @unadopted_pet = create(:pet)
+      @adopted_pet = create(:pet, :adopted)
+    end
+
+    should "return published pets where missing match" do
+      expected = [@unadopted_pet].map(&:id)
+
+      scoped = @policy
+        .apply_scope(Pet.all, type: :active_record_relation)
+        .pluck(:id)
+
+      assert_equal(scoped, expected)
+    end
   end
 
   context "#show?" do
