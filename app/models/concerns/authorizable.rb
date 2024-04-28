c7cdc79a35852cdc33b75f0b983b3776523146ac
mononoken <ken.maeshima@pm.me>
2024-04-28 15:30:36 -0700

    Add list of valid roles to authorizable

--- app/models/concerns/authorizable.rb
+++ app/models/concerns/authorizable.rb
@@ -1,60 +1,62 @@
-ADOPTER_PERMISSIONS = %i[
-  view_adopter_foster_dashboard
-  create_adopter_applications
-  view_adopter_applications
-  withdraw_adopter_applications
-  create_adopter_foster_profiles
-  manage_adopter_foster_profiles
-].freeze
-
-FOSTERER_PERMISSIONS = %i[
-  view_adopter_foster_dashboard
-  create_adopter_foster_profiles
-  manage_adopter_foster_profiles
-].freeze
-
-STAFF_PERMISSIONS = (
-  ADOPTER_PERMISSIONS.excluding(
-    %i[
-      view_adopter_foster_dashboard
-      create_adopter_applications
-      create_adopter_foster_profiles
-      manage_adopter_foster_profiles
-    ]
-  ) + %i[
-    review_adopter_applications
-    view_adopter_foster_profiles
-    purge_attachments
-    manage_default_pet_tasks
-    manage_forms
-    manage_questions
-    manage_matches
-    manage_pets
-    manage_tasks
-    view_organization_dashboard
-    manage_faqs
-  ]
-).freeze
-
-ADMIN_PERMISSIONS = (
-  STAFF_PERMISSIONS + %i[
-    activate_staff
-    invite_staff
-    manage_organization_profile
-    manage_page_text
-    manage_staff
-  ]
-).freeze
-
-PERMISSIONS = {
-  adopter: ADOPTER_PERMISSIONS,
-  fosterer: FOSTERER_PERMISSIONS,
-  staff: STAFF_PERMISSIONS,
-  admin: ADMIN_PERMISSIONS
-}.freeze
-
 module Authorizable
   extend ActiveSupport::Concern
+
+  ROLES = %w[adopter fosterer staff admin].freeze
+
+  ADOPTER_PERMISSIONS = %i[
+    view_adopter_foster_dashboard
+    create_adopter_applications
+    view_adopter_applications
+    withdraw_adopter_applications
+    create_adopter_foster_profiles
+    manage_adopter_foster_profiles
+  ].freeze
+
+  FOSTERER_PERMISSIONS = %i[
+    view_adopter_foster_dashboard
+    create_adopter_foster_profiles
+    manage_adopter_foster_profiles
+  ].freeze
+
+  STAFF_PERMISSIONS = (
+    ADOPTER_PERMISSIONS.excluding(
+      %i[
+        view_adopter_foster_dashboard
+        create_adopter_applications
+        create_adopter_foster_profiles
+        manage_adopter_foster_profiles
+      ]
+    ) + %i[
+      review_adopter_applications
+      view_adopter_foster_profiles
+      purge_attachments
+      manage_default_pet_tasks
+      manage_forms
+      manage_questions
+      manage_matches
+      manage_pets
+      manage_tasks
+      view_organization_dashboard
+      manage_faqs
+    ]
+  ).freeze
+
+  ADMIN_PERMISSIONS = (
+    STAFF_PERMISSIONS + %i[
+      activate_staff
+      invite_staff
+      manage_organization_profile
+      manage_page_text
+      manage_staff
+    ]
+  ).freeze
+
+  PERMISSIONS = {
+    adopter: ADOPTER_PERMISSIONS,
+    fosterer: FOSTERER_PERMISSIONS,
+    staff: STAFF_PERMISSIONS,
+    admin: ADMIN_PERMISSIONS
+  }.freeze
 
   included do
     rolify
